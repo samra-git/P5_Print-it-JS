8675f3aa2bdff4344cde4ab250360c284b4f7d71
Samra Harbi <samra.harbi78@gmail.com>
2023-10-30 20:52:16 +0100

    correct lien

--- assets/script.js
+++ assets/script.js
@@ -12,20 +12,20 @@ let allDots = [];
 
 const slides = [
   {
-    image: "./assets/images/slideshow/slide1.jpg",
+    image: "./images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "./assets/images/slideshow/slide2.jpg",
+    image: "./images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "./assets/images/slideshow/slide3.jpg",
+    image: "./images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "./assets/images/slideshow/slide4.png",
+    image: "./images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
